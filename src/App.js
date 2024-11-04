4530d336ae0b02c1c07ff512969faecf781d572a
Antony Michael Anson(UST <287930@ust.com>
2024-11-04 16:01:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Login from './pages/Auth/Login'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </Router>
+
+    </div>
+  );
+}
+
+
+export default App;
+
+
+// import {Button} from 'antd'
+// import {motion} from 'framer-motion'
+// function App() {
+//   return (
+//     <div className="App">
+//       <p>Carvb</p>
+//       <button className='btn btn-primary'>Login</button>
+//       <Button type="primary">Button</Button>
+      
+//       <motion.div className="my-test"
+//         initial={{x:'-100%',opacity:0}}
+//         animate={{x:0, opacity:1}}
+//         transition={{ duration: 0.5 }}
+//       >
+//         <h1>Hi</h1>
+//         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quia laborum veritatis quo similique, saepe eveniet quos, assumenda velit alias, cumque aperiam exercitationem odit. Modi voluptas mollitia adipisci earum, delectus repudiandae consequuntur, accusamus tempora culpa at ipsum quidem eius aliquam est sit cupiditate amet! Natus accusantium voluptatibus eveniet rem nemo!</p>
+//       </motion.div>
+
+//     </div>
+//   );
+// }
